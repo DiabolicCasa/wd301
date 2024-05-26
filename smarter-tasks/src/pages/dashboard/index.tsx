8f4495d9d31a51e3b10096d0fe29a94936da01df
gautam-bhagat <gautambhagat.business@gmail.com>
2024-05-26 23:45:17 +0530

    displaying name and email

--- smarter-tasks/src/pages/dashboard/index.tsx
+++ smarter-tasks/src/pages/dashboard/index.tsx
@@ -8,11 +8,19 @@ const Dashboard: React.FC = () => {
     localStorage.removeItem("userData");
     navigate("/signin")
   };
+
+  const user= JSON.parse(localStorage.getItem("userData") || "")
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">
         Dashboard
       </h1>
+      <p>
+        {
+            `${user.name}
+            ${user.email}`
+        }
+      </p>
       <button className="bg-red-500 p-2 rounded-md text-white"
         id="logout-link"
         onClick={() => {
