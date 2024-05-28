d4a06b5203bf1b1f29467a8c99fec59c02057fda
gautam-bhagat <gautambhagat.business@gmail.com>
2024-05-28 21:23:15 +0530

    drag drop enabled

--- smarter-tasks/src/main.tsx
+++ smarter-tasks/src/main.tsx
@@ -2,9 +2,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from "./context/theme.tsx";
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
-);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <App />
+)
--- smarter-tasks/src/pages/project_details/ProjectDetails.tsx
+++ smarter-tasks/src/pages/project_details/ProjectDetails.tsx
@@ -2,10 +2,16 @@ import {  useParams } from "react-router-dom";
 import { useProjectsState } from "../../context/projects/context";
 import React from "react";
 import { Link } from "react-router-dom";
+import { useTasksState } from "../../context/task/context";
+import DragDropList from "./DragDropList";
 
 const ProjectDetails :React.FC = () => {
   const projectState = useProjectsState();
   const { projectID } = useParams();
+
+  const tasksState = useTasksState();
+
+
 
   const selectedProject = projectState?.projects.filter(
     (project) => `${project.id}` === projectID
@@ -13,6 +19,10 @@ const ProjectDetails :React.FC = () => {
 
   if (!selectedProject) {
     return <>No such Project!</>;
+  }
+
+  if (tasksState.isLoading) {
+    return <>Loading...</>;
   }
   return (
     <>
@@ -23,14 +33,15 @@ const ProjectDetails :React.FC = () => {
         <Link to={`tasks/new`}>
           <button
             id="newTaskBtn"
-            className="rounded-md bg-green-600 px-4 py-2 m-2 text-sm font-medium text-white hover:bg-opacity-95 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+            className="rounded-md bg-blue-600 px-4 py-2 m-2 text-sm font-medium text-white hover:bg-opacity-95 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
           >
             New Task
           </button>
         </Link>
       </div>
-
-     
+      <div className="grid grid-cols-1 gap-2">
+        <DragDropList data={tasksState.projectData} />
+      </div>
     </>
   );
 };
